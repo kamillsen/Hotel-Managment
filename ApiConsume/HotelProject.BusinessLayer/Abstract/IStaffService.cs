9a35b36eb284b63da1a9f5fbcf74d2d9a172e21b
kamillsen <senkamil1864mail.com>
2024-11-02 20:28:12 +0300

    İlk commit

---
+++ ApiConsume/HotelProject.BusinessLayer/Abstract/IStaffService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HotelProject.DataAccessLayer.Concrete;
+using HotelProject.EntityLayer.Concrete;
+
+namespace HotelProject.BusinessLayer.Abstract
+{
+    public interface IStaffService : IGenericService<Staff>
+    {
+        int TGetStaffCount();
+        List<Staff> TLast4Staff();
+
+    }
+}
---
+++ RapidAPI/RapidApiConsume/Controllers/BookingByCityController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RapidApiConsume.Models;
+
+namespace RapidApiConsume.Controllers
+{
+    public class BookingByCityController : Controller
+    {
+        public async Task<IActionResult> Index(string cityID)
+        {
+
+            if (!string.IsNullOrEmpty(cityID))
+            {
+                var client = new HttpClient();
+                var request = new HttpRequestMessage
+                {
+                    Method = HttpMethod.Get,
+                    RequestUri = new Uri($"https://booking-com.p.rapidapi.com/v2/hotels/search?dest_id={cityID}&order_by=popularity&checkout_date=2025-01-19&children_number=2&filter_by_currency=EUR&locale=en-gb&dest_type=city&checkin_date=2025-01-18&categories_filter_ids=class%3A%3A2%2Cclass%3A%3A4%2Cfree_cancellation%3A%3A1&children_ages=5%2C0&include_adjacency=true&page_number=0&adults_number=2&room_number=1&units=metric"),
+                    Headers =
+    {
+        { "x-rapidapi-key", "b6b83d40aemsh1f5d9829888cf9ep18771ejsn832d06e3ae74" },
+        { "x-rapidapi-host", "booking-com.p.rapidapi.com" },
+    },
+                };
+                using (var response = await client.SendAsync(request))
+                {
+                    response.EnsureSuccessStatusCode();
+                    var body = await response.Content.ReadAsStringAsync();
+                    var values = JsonConvert.DeserializeObject<BookingApiViewModel>(body);
+                    return View(values.results.ToList());
+                }
+            }
+            else 
+            {
+                var client = new HttpClient();
+                var request = new HttpRequestMessage
+                {
+                    Method = HttpMethod.Get,
+                    RequestUri = new Uri("https://booking-com.p.rapidapi.com/v2/hotels/search?dest_id=-1456928&order_by=popularity&checkout_date=2025-01-19&children_number=2&filter_by_currency=EUR&locale=en-gb&dest_type=city&checkin_date=2025-01-18&categories_filter_ids=class%3A%3A2%2Cclass%3A%3A4%2Cfree_cancellation%3A%3A1&children_ages=5%2C0&include_adjacency=true&page_number=0&adults_number=2&room_number=1&units=metric"),
+                    Headers =
+    {
+        { "x-rapidapi-key", "b6b83d40aemsh1f5d9829888cf9ep18771ejsn832d06e3ae74" },
+        { "x-rapidapi-host", "booking-com.p.rapidapi.com" },
+    },
+                };
+                using (var response = await client.SendAsync(request))
+                {
+                    response.EnsureSuccessStatusCode();
+                    var body = await response.Content.ReadAsStringAsync();
+                    var values = JsonConvert.DeserializeObject<BookingApiViewModel>(body);
+                    return View(values.results.ToList());
+                }
+            }
+
+             
+        }
+    }
+}
